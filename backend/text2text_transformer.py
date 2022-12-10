3f12813aa06be28c9375a92f85061626c2aed557
Arijit Das <arijitd@gmail.com>
2022-12-10 20:26:32 +0100

    text2speech interface

--- backend/text2text_transformer.py
+++ backend/text2text_transformer.py
@@ -1,12 +1,12 @@
 import requests
 
 
-def Flan_T5_Transformer(text: str, api_key: str):
-    API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-large"
+def T5_Transformer(text: str, api_key: str):
+    API_URL = "https://api-inference.huggingface.co/models/t5-large"
     headers = {"Authorization": "Bearer " + api_key}
     input = {'inputs': text}
 
     # Request a translation
     response = requests.post(API_URL, headers=headers, json=input)
 
-    return response.json()[0].values()[0]
+    return response.json()[0]['translation_text']
