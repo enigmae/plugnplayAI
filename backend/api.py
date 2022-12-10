85228b3647ac320a3e69d8ea1e420fed104f73a2
Arijit Das <arijitd@gmail.com>
2022-12-10 19:38:01 +0100

    text2speech interface

--- backend/api.py
+++ backend/api.py
@@ -3,10 +3,11 @@ from transcriptor import transcribe as _transcribe
 from text2text_transformer import Flan_T5_Transformer
 from chatGPT import ask
 from image_generator import generate_image as _generate_image
-
+from text2speech import fastspeech
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+huggingface_api_key = "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"  # TODO pass this token as a secret
 
 origins = ["*"]
 
@@ -41,9 +42,8 @@ async def translate(text_file: UploadFile):
     """
     Translate some text.
     """
-    api_key = "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"  # TODO pass this token as a secret
     prefix = "translate English to German: "
-    return Flan_T5_Transformer(prefix + text_file.file.read().decode(), api_key)
+    return Flan_T5_Transformer(prefix + text_file.file.read().decode(), huggingface_api_key)
 
 
 @app.post("/summarize", response_model=str)
@@ -51,17 +51,16 @@ async def summarize(text_file: UploadFile):
     """
     Summarize some text.
     """
-    api_key = "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"  # TODO pass this token as a secret
     prefix = "summarize: "
-    return Flan_T5_Transformer(prefix + text_file.file.read().decode(), api_key)
+    return Flan_T5_Transformer(prefix + text_file.file.read().decode(), huggingface_api_key)
 
 
 @app.post("/chatbot", response_model=str)
 async def question_answering(question: str):
-   """
-   Translate some text.
-   """
-   return ask(question)
+    """
+    Answer questions using chatGPT
+    """
+    return ask(question)
 
 
 @app.post("/generate_image")
@@ -72,3 +71,11 @@ async def generate_image(prompt: str):
     api_key = 'sk-3DEERMlkvdTymBFocEI9uGlEV610ro1hL3MgzfYQ7zhCnsKX'  # TODO pass this token as a secret
     image_bytes: bytes = _generate_image(prompt, api_key)
     return Response(content=image_bytes, media_type="image/png")
+
+
+@app.post("/text2speech", response_model=str)
+async def text2speech(text: str):
+    """
+    Convert text to speech
+    """
+    return fastspeech(text, huggingface_api_key)
