fd50228121e6062eb0be4acbeea9bb1c69a97063
Arijit Das <arijitd@gmail.com>
2022-12-10 15:01:54 +0100

    huggingface translate api

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile
 from transcriptor import transcribe as _transcribe
+from translator import translate as _translate
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -31,3 +32,12 @@ async def transcribe(audio_mp3: UploadFile):
     """
     api_key = 'b2bcc06df08d4245950139b798fd5e36'  # TODO pass this token as a secret
     return _transcribe(audio_mp3.file.read(), api_key)
+
+
+@app.post("/translator", response_model=str)
+async def translate(text_file: UploadFile):
+    """
+    Translate some text.
+    """
+    api_key = "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"  # TODO pass this token as a secret
+    return _translate(text_file.file.read().decode(), api_key)
---
+++ backend/translator.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def translate(text: str, api_key: str):
+    API_URL = "https://api-inference.huggingface.co/models/t5-large"
+    headers = {"Authorization": "Bearer " + api_key}
+    input = {'inputs': text}
+
+    # Request a translation
+    response = requests.post(API_URL, headers=headers, json=input)
+
+    return response.json()["translation_text"]
