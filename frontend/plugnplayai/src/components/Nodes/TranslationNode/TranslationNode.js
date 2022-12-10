bb431042291c1c7418658bddbd26d91cf580f230
swayam1998 <swayam1998@gmail.com>
2022-12-10 23:36:18 +0530

    implements translation node

--- frontend/plugnplayai/src/components/Nodes/TranslationNode/TranslationNode.js
+++ frontend/plugnplayai/src/components/Nodes/TranslationNode/TranslationNode.js
@@ -1,5 +1,7 @@
 import { Handle, Position } from 'reactflow';
-import { Group, Text, TextInput } from '@mantine/core';
+import { Button, Group, Loader, LoadingOverlay, Text, Textarea, TextInput } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import axiosInstance from '../../../services/axiosInstance';
 
 
 function TranslationNode({ data }) {
@@ -7,21 +9,77 @@ function TranslationNode({ data }) {
     const baseColor = model.color;
     const handleSize = 15;
 
+    const [loading, setLoading] = useState(false);
+    const [responseData, setResponseData] = useState(null);
+
+    useEffect(() => {
+        console.log(data)
+    }, [data])
+
+    const processTranslate = async () => {
+        setLoading(true);
+
+        const form = new FormData();
+        form.append('text_file', data.sourceData);
+
+        const response = await axiosInstance.post('/translate', form, {
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+
+        setLoading(false);
+        setResponseData(response.data);
+    }
+
     return (
         <>
-            <Handle type="source" position={Position.Left} id="a" style={{ width: handleSize, height: handleSize }} />
-            <div style={{ border: `2px solid ${baseColor}`, borderRadius: 5, width: 300, height: 200 }}>
+            <Handle
+                type="target"
+                position={Position.Left}
+                id="a"
+                style={{ width: handleSize, height: handleSize }}
+                onConnect={(params) => console.log('handle target onConnect', params)}
+            />
+            <div style={{ border: `2px solid ${baseColor}`, borderRadius: 5, width: 300, paddingBottom: 10 }}>
                 <div style={{ display: 'flex', justifyContent: 'center', borderBottom: `2px solid ${baseColor}`, background: baseColor, padding: 8 }}>
                     <Text color='white' weight={800} size='xl'>{model.name}</Text>
                 </div>
-                <div style={{ padding: 8 }}>
-                    <Group position='apart' style={{ padding: 8 }}>
-                        <Text>Seed</Text>
-                        <TextInput />
-                    </Group>
+                <div style={{ padding: 8, flex: 1 }}>
+                    <div>
+                        <Textarea
+                            minRows={4}
+                            value={data.sourceData ? data.sourceData : ''}
+                        />
+                    </div>
                 </div>
+                <div style={{ display: 'flex', flex: 1, justifyContent: 'center' }}>
+                    <Button
+                        variant="outline"
+                        style={{ width: 150, borderColor: baseColor, color: baseColor }}
+                        onClick={() => processTranslate()}
+                        disabled={!data.sourceData}
+                    >
+                        {loading ? <Loader variant="bars" size="xs" color='green' /> : 'Apply'}
+                    </Button>
+                </div>
+                {responseData && (
+                    <div style={{ padding: 10 }}>
+                        <Textarea
+                            minRows={5}
+                            value={responseData}
+                            onChange={(ev) => setResponseData(ev.target.value)}
+                        />
+                    </div>
+                )}
             </div>
-            <Handle type="target" position={Position.Right} style={{ width: handleSize, height: handleSize }} />
+            <Handle
+                type="source"
+                position={Position.Right}
+                style={{ width: handleSize, height: handleSize }}
+                onConnect={(params) => console.log('handle onConnect', params)}
+            />
         </>
     );
 }
