bbcda7a88e708df834e01893bfb3b4050b774895
swayam1998 <swayam1998@gmail.com>
2022-12-10 13:17:54 +0530

    adds models menu and model buttons

--- frontend/plugnplayai/src/App.js
+++ frontend/plugnplayai/src/App.js
@@ -5,14 +5,7 @@ function App() {
   return (
     <div style={{ height: '100vh', width: '100hw' }}>
       <MantineProvider withGlobalStyles withNormalizeCSS>
-        <div style={{ display: 'flex', flexDirection: 'row', height: '100vh', width: '100hw' }}>
-          <div style={{ display: 'flex', height: '100%', width: '15%', border: '1px solid black' }}>
-            heloss
-          </div>
-          <div style={{ height: '100%', width: '85%', border: '1px solid red' }}>
-            <Flow />
-          </div>
-        </div>
+        <Flow />
       </MantineProvider>
     </div>
   );
--- frontend/plugnplayai/src/components/Flow/Flow.js
+++ frontend/plugnplayai/src/components/Flow/Flow.js
@@ -1,24 +1,26 @@
+import { Button, Text } from '@mantine/core';
 import { useState, useCallback } from 'react';
 import ReactFlow, { addEdge, Controls, Background, applyNodeChanges, applyEdgeChanges } from 'reactflow';
 import 'reactflow/dist/style.css';
 import SpeechToTextNode from '../SpeechToTextNode/SpeechToTextNode.js';
+import { MessageChatbot, Microphone2, Mountain, TextRecognition } from 'tabler-icons-react';
 
 const initialNodes = [
     {
         id: '1',
         data: { label: 'Hello' },
-        position: { x: 0, y: 0 },
+        position: { x: 550, y: 350 },
         type: 'input',
     },
     {
         id: '2',
         data: { label: 'World' },
-        position: { x: 100, y: 100 },
+        position: { x: 600, y: 600 },
     },
     {
         id: 'node-1',
         type: 'textUpdater',
-        position: { x: 0, y: 0 },
+        position: { x: 200, y: 200 },
         data: { value: 123 }
     },
 
@@ -45,20 +47,89 @@ function Flow() {
         [setEdges]
     );
 
+    const ModelButton = ({ icon, modelName }) => {
+        return (
+            <div style={{ padding: '10px 10px 0px 10px', display: 'flex' }}>
+                <Button
+                    variant='outline'
+                    style={{ display: 'flex', flex: 1, border: '1px solid #e5e5e5', color: 'black', padding: 0 }}
+                >
+                    <div style={{ width: '160px', flex: 1, display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
+                        <div style={{ padding: '0px 10px 0px 10px' }}>
+                            {icon}
+                        </div>
+                        <div style={{ display: 'flex', flex: 1, justifyContent: 'center', }}>
+                            <Text size='md' weight={400} >
+                                {modelName}
+                            </Text>
+                        </div>
+                    </div>
+                </Button>
+            </div>
+        )
+    }
+
     return (
+
         <div style={{ height: '100%' }}>
-            <ReactFlow
-                nodes={nodes}
-                edges={edges}
-                nodeTypes={nodeTypes}
-                onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                onConnect={onConnect}
-            >
-                <Background />
-                <Controls />
-            </ReactFlow>
-        </div>
+            <div style={{ display: 'flex', flexDirection: 'row', height: '100vh', width: '100hw' }}>
+                {/* Model Menu */}
+                <div style={{ display: 'flex', flexDirection: 'column', background: '#FCFDFD', height: '100%', width: '15%', borderRight: '1px solid #efefef' }}>
+                    <div style={{ color: '#333333', fontSize: 25, textAlign: 'center' }}>
+                        Models
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
+                        <ModelButton
+                            icon={<Microphone2
+                                size={25}
+                                strokeWidth={1.5}
+                                color={'#ffaa00'}
+                            />}
+                            modelName="Speech-to-Text"
+                        />
+                        <ModelButton
+                            icon={<TextRecognition
+                                size={25}
+                                strokeWidth={1.5}
+                                color={'#7dd279'}
+                            />}
+                            modelName="Text-to-Text"
+                        />
+                        <ModelButton
+                            icon={<Mountain
+                                size={25}
+                                strokeWidth={1.5}
+                                color={'#bf40af'}
+                            />}
+                            modelName="Text-to-Image"
+                        />
+                        <ModelButton
+                            icon={<MessageChatbot
+                                size={25}
+                                strokeWidth={1.5}
+                                color={'#ff3f3f'}
+                            />}
+                            modelName="Text-to-Chat"
+                        />
+                    </div>
+                </div>
+
+                {/* ReactFlow  */}
+                <div style={{ height: '100%', width: '85%' }}>
+                    <ReactFlow
+                        nodes={nodes}
+                        edges={edges}
+                        nodeTypes={nodeTypes}
+                        onNodesChange={onNodesChange}
+                        onEdgesChange={onEdgesChange}
+                        onConnect={onConnect}
+                    >
+                        <Background />
+                        <Controls />
+                    </ReactFlow>
+                </div>
+            </div>
+        </div >
     );
 }
 
