90f23bd19eba90e583ad6a64b50b50b0e24d8c41
Arijit Das <arijitd@gmail.com>
2022-12-11 14:47:50 +0100

    chatgpt test

--- backend/chatGPT.py
+++ backend/chatGPT.py
@@ -1,9 +1,9 @@
-from pychatgpt import Chat, Options
-
-
-def ask(question: str):
-    options = Options()
-    options.track = True
-    chat = Chat(email="arijit.das@selfsupervised.de", password="5aLpF:zmY7V4T39", options=options)
-    answer, _, _ = chat.ask(question)
-    return answer
+# from pychatgpt import Chat, Options
+#
+#
+# def ask(question: str):
+#     options = Options()
+#     options.track = True
+#     chat = Chat(email="arijit.das@selfsupervised.de", password="5aLpF:zmY7V4T39", options=options)
+#     answer, _, _ = chat.ask(question)
+#     return answer
